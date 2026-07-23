798a9b169c200d679f8db05149b9d783c5ef80ec
DevMak <sarunimark06@gmail.com>
2026-07-23 11:49:26 +0300

    add account handler to chi router

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -1,1 +1,20 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func NewRouter(accountHandler *AccountHandler) http.Handler {
+	r := chi.NewRouter()
+
+	r.Route("/accounts", func(r chi.Router) {
+		r.Post("/", accountHandler.CreateAccount)
+		r.Get("/{customer_id}", accountHandler.GetCustomerAccounts)
+		r.Put("/deposit", accountHandler.Deposit)
+		r.Put("/withdraw", accountHandler.Withdraw)
+	})
+
+	return r
+}
